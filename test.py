9915251f85c9ee0a3802d6f8d35e296949fedc8d
danieloflynn <danielpoflynn@gmail.com>
2024-11-03 23:53:30 +0000

    put csv in hadoop

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-from pyspark import SparkContext
-import sys
+from pyspark.sql import SparkSession
 
-if __name__ == "__main__":
-    sc = SparkContext(appName="RDD_Examples")
-    my_list = sc.parallelize([1, 2, 3, 4, 5])
-    my_other_list = sc.parallelize([x for x in range(0, 1)])
-    # tweets = sc.textFile("hdfs:///user/cbw/a1_tweets.txt") # load data from HDFS
-    words = sc.textFile("hdfs://localhost:9000/test/test.txt") # load data from HDFS
-    print(words.collect())
+filepath = "fetal_health.csv"
+
+# Create a Spark session
+spark = SparkSession.builder.appName("WriteToHDFS").getOrCreate()
+
+# Create a DataFrame
+df = spark.read.format("csv").option("header", "true").load(filepath)
